1724ae94bfeae7bd66243a97925f86b0c3e1bfcf
jacob679213 <47827860+jacob679213@users.noreply.github.com>
2020-10-05 13:47:35 -0400

    began to build page

--- client.js
+++ client.js
@@ -1,1 +1,10 @@
 console.log('script loaded')
+
+//add click event hangler on button
+let $button = document.getElementById("addTodo")
+$button.onclick = addTodo()
+
+//define the button click handler
+function addTodo(){
+    console.log("button was clicked")
+}
