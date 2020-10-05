5ed116a6ef1eb2d6e86e25b5a90126e8d22ebff1
jacob679213 <47827860+jacob679213@users.noreply.github.com>
2020-10-05 14:25:26 -0400

    Finished todo list, started working on Header

--- client.js
+++ client.js
@@ -1,10 +1,32 @@
-console.log('script loaded')
+//console.log('script loaded')
 
 //add click event hangler on button
-let $button = document.getElementById("addTodo")
-$button.onclick = addTodo()
+let $button = document.getElementById('addTodo')
+let $textBox = document.getElementById('inputBox')
+let $list = document.getElementById('list')
+$button.onclick = addTodo
 
 //define the button click handler
-function addTodo(){
-    console.log("button was clicked")
+function addTodo(event){
+    //console.log("button was clicked")
+    //$button.style.backgroundColor = 'red'
+    
+    //get text from input
+    let newTodoText = $textBox.value
+    console.log(newTodoText)
+    //create new list item
+    let $newTodoItem = document.createElement('li') 
+    $newTodoItem.innerHTML = newTodoText
+    //put item in list
+    $list.append($newTodoItem)
+}
+
+//variable for heading element
+let $heading = document.getElementByid('heading')
+//set 'changeColor' function as click handler on header
+$heading.onclick = changeColor
+//define a 'changeColor' function
+
+function changeColor(event) {
+    $heading.style.color = 'red'
 }
